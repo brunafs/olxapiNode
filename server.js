aff1d49c2c2993c957b6b50a8b0bb53863b081c5
Bruna Santana <brunasantana98@gmail.com>
2021-01-13 14:38:17 -0300

    Rotas criadas + Arquivos Controllers

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 
+const apiRoutes = require('./src/routes');
+
 mongoose.connect(process.env.DATABASE, {
    useNewUrlParser: true,
    useFindAndModify: false,
@@ -23,9 +25,7 @@ server.use(fileupload());
 
 server.use(express.static(__dirname+'/public'));
 
-server.get('/ping', (req, res) => {
-   res.json({pong: true});
-});
+server.use('/', apiRoutes);
 
 server.listen(process.env.PORT, () => {
    console.log(`- Rodando no endereço: ${process.env.BASE}`);
